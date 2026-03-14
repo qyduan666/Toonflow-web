12f7e43f3db612e2d00e960afa8e99a7a0b31d73
小帅 <2944435683>
2026-03-14 15:51:03 +0800

    分镜弹窗

--- src/components/migrateShow.vue
+++ src/components/migrateShow.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="migrateShow">
-    <t-dialog
-      :visible.sync="visible"
-      header="迁移数据"
-      width="500px"
-      :onClose="() => (visible = false)"
-      @confirm="onConfirm"
-      @cancel="() => (visible = false)">
+    <t-dialog :visible.sync="visible" header="迁移数据" width="500px" :maskClosable="false" :closeBtn="false">
       <div class="taskList">
         <div class="item">
           <span>检测到您有旧版本的数据，是否需要迁移？</span>
         </div>
       </div>
+      <template #footer>
+        <t-space>
+          <t-button theme="default" @click="() => (visible = false)">不在显示</t-button>
+          <t-button theme="primary" @click="onConfirm">确定</t-button>
+        </t-space>
+      </template>
     </t-dialog>
   </div>
 </template>
@@ -30,7 +30,7 @@ function onConfirm() {
       MessagePlugin.error("数据迁移失败");
     })
     .finally(() => {
-    //   visible.value = false; // 关闭对话框
+      //   visible.value = false; // 关闭对话框
     });
 }
 </script>
--- src/views/production/components/storyboard/index.vue
+++ src/views/production/components/storyboard/index.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="storyboard">
     <t-dialog mode="full-screen" :footer="false" :header="false" :closeBtn="false" v-model:visible="addStoryboardShow" class="fullscreenDialog">
-      <div class="closure">
-        <i-close-small theme="outline" size="24" fill="#4a4a4a" @click="addStoryboardShow = false" />
+      <div class="closure" @click="addStoryboardShow = false">
+        <i-close-small theme="outline" size="24" fill="#4a4a4a" />
+      </div>
+      <div class="data">
+        <div class="open c" v-if="!openShowVisible && !isLeaving">
+          <i-expand-text-input
+            theme="outline"
+            size="24"
+            fill="#4a4a4a"
+            @click="
+              () => {
+                openShowVisible = true;
+              }
+            " />
+        </div>
+        <transition name="slide" @before-leave="isLeaving = true" @after-leave="isLeaving = false">
+          <floatingTaskBox v-model="openShowVisible" v-if="openShowVisible" />
+        </transition>
       </div>
     </t-dialog>
   </div>
@@ -10,21 +26,43 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
+import floatingTaskBox from "./components/floatingTaskBox.vue";
 const addStoryboardShow = defineModel({
   type: Boolean,
   default: false,
 });
+const openShowVisible = ref(false);
+const isLeaving = ref(false);
 </script>
 
 <style lang="scss" scoped>
 .storyboard {
   .fullscreenDialog {
+    width: 100%;
+    height: 100%;
     .closure {
       position: absolute;
       top: 10px;
       right: 10px;
       z-index: 9999;
       cursor: pointer;
+    }
+    .data {
+      width: 100%;
+      height: 100%;
+      position: relative;
+      overflow: hidden;
+      .open {
+        position: absolute;
+        top: 60px;
+        right: 10px;
+        z-index: 9999;
+        width: 40px;
+        height: 40px;
+        background-color: #f0f0f0;
+        border-radius: 10px;
+        cursor: pointer;
+      }
     }
   }
   :deep(.fullscreenDialog) {
@@ -37,6 +75,19 @@ const addStoryboardShow = defineModel({
     .t-dialog__wrap {
       padding: 0 !important;
     }
+    .t-dialog__footer {
+      display: none !important;
+    }
   }
+}
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform 0.3s ease-out;
+}
+.slide-enter-from {
+  transform: translateX(100%);
+}
+.slide-leave-to {
+  transform: translateX(100%);
 }
 </style>
